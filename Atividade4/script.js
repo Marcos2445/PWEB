6ebd5e24453799b3caeb8216125d8deb910e284b
Marcos2445 <69857352+Marcos2445@users.noreply.github.com>
2021-08-23 17:06:53 -0300

    Add files via upload

---
+++ Atividade4/script.js
@@ -0,0 +1,13 @@
+function abrirnav(){
+  document.getElementById('barraLat').style.height='100%';
+  document.getElementById('sect').style.marginLeft='150px';
+
+}
+function fecharnav(){
+    document.getElementById('barraLat').style.height='0%';
+    document.getElementById('sect').style.marginLeft='0px';
+}
+
+
+
+
