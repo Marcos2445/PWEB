a1efc119ebdeae7c6177b85b11e28d3cc8d64538
Marcos2445 <69857352+Marcos2445@users.noreply.github.com>
2021-10-18 11:51:28 -0300

    Add files via upload

---
+++ Atividade10/ativ10.js
@@ -0,0 +1,18 @@
+
+
+////1
+var Aluno1 = new Object(); 
+Aluno1.RA = "0030482011044"; 
+Aluno1.Nome = "Marcos Parreira Junior";
+
+////2
+var Aluno1 = {}; 
+Aluno1.RA = "0030482011044"; 
+Aluno1.Nome = "Marcos Parreira Junior";
+
+///3
+var aluno = { 
+    Nome : "Marcos Parreira Junior",
+    RA: "0030482011044",
+    
+    };
