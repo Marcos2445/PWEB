cdd4e45dc3da735afcb1900c1c1b345da8fba4fa
Marcos2445 <69857352+Marcos2445@users.noreply.github.com>
2021-10-21 18:05:04 -0300

    Add files via upload

---
+++ Portifolio/script.js
@@ -0,0 +1,40 @@
+function crescerbarrajs(){
+    
+    var img = window.document.getElementById('cadfechadojs');
+    img.setAttribute('src', 'cadabert.png');
+    img.setAttribute('alt', 'cadeado aberto')
+    document.getElementById('barrajs').style.width = '300px';
+
+   }
+   function crescerbarrahtml(){
+    
+    var img = window.document.getElementById('cadfechadohtml');
+    img.setAttribute('src', 'cadabert.png');
+    img.setAttribute('alt', 'cadeado aberto')
+    document.getElementById('barrahtml').style.width = '350px';
+
+   }
+   function crescerbarracss(){
+    
+    var img = window.document.getElementById('cadfechadocss');
+    img.setAttribute('src', 'cadabert.png');
+    img.setAttribute('alt', 'cadeado aberto')
+    document.getElementById('barracss').style.width = '320px';
+
+   }
+   function crescerbarrasql(){
+    
+    var img = window.document.getElementById('cadfechadosql');
+    img.setAttribute('src', 'cadabert.png');
+    img.setAttribute('alt', 'cadeado aberto')
+    document.getElementById('barrasql').style.width = '330px';
+
+   }
+   function crescerbarrajava(){
+    
+    var img = window.document.getElementById('cadfechadojava');
+    img.setAttribute('src', 'cadabert.png');
+    img.setAttribute('alt', 'cadeado aberto')
+    document.getElementById('barrajava').style.width = '200px';
+
+   }
