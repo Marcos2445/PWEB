861ac936b75db6ac0c3301bf80bd6722b38e25c6
Marcos2445 <69857352+Marcos2445@users.noreply.github.com>
2021-10-21 23:56:36 -0300

    Add files via upload

--- Portifolio/script.js
+++ Portifolio/script.js
@@ -4,7 +4,7 @@ function crescerbarrajs(){
     img.setAttribute('src', 'cadabert.png');
     img.setAttribute('alt', 'cadeado aberto')
     document.getElementById('barrajs').style.width = '300px';
-
+    document.getElementById('barrajs').textContent='80%';
    }
    function crescerbarrahtml(){
     
@@ -12,7 +12,7 @@ function crescerbarrajs(){
     img.setAttribute('src', 'cadabert.png');
     img.setAttribute('alt', 'cadeado aberto')
     document.getElementById('barrahtml').style.width = '350px';
-
+    document.getElementById('barrahtml').textContent='90%';
    }
    function crescerbarracss(){
     
@@ -20,6 +20,7 @@ function crescerbarrajs(){
     img.setAttribute('src', 'cadabert.png');
     img.setAttribute('alt', 'cadeado aberto')
     document.getElementById('barracss').style.width = '320px';
+    document.getElementById('barracss').textContent='60%';
 
    }
    function crescerbarrasql(){
@@ -28,6 +29,7 @@ function crescerbarrajs(){
     img.setAttribute('src', 'cadabert.png');
     img.setAttribute('alt', 'cadeado aberto')
     document.getElementById('barrasql').style.width = '330px';
+    document.getElementById('barrasql').textContent='85%';
 
    }
    function crescerbarrajava(){
@@ -36,5 +38,60 @@ function crescerbarrajs(){
     img.setAttribute('src', 'cadabert.png');
     img.setAttribute('alt', 'cadeado aberto')
     document.getElementById('barrajava').style.width = '200px';
+    document.getElementById('barrajava').textContent='40%';
 
    }
+   function mudarcara(){
+    
+    var img = window.document.getElementById('Eu');
+    img.setAttribute('src', 'cabecacs31.png');
+    img.setAttribute('alt', 'eu sorrindo')
+ 
+
+   }
+  //function fala(){
+    
+    //document.getElementById('falainv').style.color= 'white';
+   
+
+  // }
+   function fala1(){
+    document.getElementById('balfala1').textContent='Primeiramente ótima escolha, agora aqui embaixo você pode ver meu nível de conhecimento em algumas coisas. Você precisa quebrar a segurança para ver meus dados      (só clicar nos cadeados)';
+    var img = window.document.getElementById('bocaaberta');
+    img.setAttribute('src', 'bocaaberta3.png');
+    img.setAttribute('alt', 'eu ')
+   
+   }
+   function fala1errada(){
+    document.getElementById('balfala1').textContent='Cara, clica em mim e não no balão';
+    var img = window.document.getElementById('bocaaberta');
+    img.setAttribute('src', 'bocaaberta31.png');
+    img.setAttribute('alt', 'eu bravo')
+   }
+   function fala2(){
+    document.getElementById('balfala2').textContent='Agora vou mostrar alguns outros trabalhos meus em web';
+    var img = window.document.getElementById('bocaaberta2');
+    img.setAttribute('src', 'bocaaberta3.png');
+    img.setAttribute('alt', 'eu')
+   }
+   function fala2errada(){
+    document.getElementById('balfala2').textContent='??? em mim não no balão';
+    var img = window.document.getElementById('bocaaberta2');
+    img.setAttribute('src', 'bocaaberta31.png');
+    img.setAttribute('alt', 'eu bravo')
+   }
+   function fala3(){
+    document.getElementById('balfala3').textContent='Aqui vão alguns meios pelos quais você consegue entrar em contato comigo caso queira';
+    var img = window.document.getElementById('bocaaberta3');
+    img.setAttribute('src', 'bocaaberta3.png');
+    img.setAttribute('alt', 'eu')
+   }
+   function fala3errada(){
+    document.getElementById('balfala3').textContent='Em mim...';
+    var img = window.document.getElementById('bocaaberta3');
+    img.setAttribute('src', 'bocaaberta31.png');
+    img.setAttribute('alt', 'eu bravo')
+   }
+
+
+
