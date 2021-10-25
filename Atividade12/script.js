4284b3b53e6861535ac46c927fc38199f36bf727
Marcos2445 <69857352+Marcos2445@users.noreply.github.com>
2021-10-25 12:51:13 -0300

    Add files via upload

---
+++ Atividade12/script.js
@@ -0,0 +1,19 @@
+function abrirjanela(){
+   var img;
+   img = document.getElementById('janela');
+   img.setAttribute ('src', 'janabert.png');
+   img.setAttribute ('alt', 'janela aberta');
+}
+function fechajanela(){
+    var img;
+    img = document.getElementById('janela');
+    img.setAttribute ('src', 'janfech.png');
+    img.setAttribute ('alt', 'janela fechada');
+    
+ }
+ function quebra(){
+    var img;
+    img = document.getElementById('janela');
+    img.setAttribute ('src', 'janqueb.png');
+    img.setAttribute ('alt', 'janela quebrada');
+ }
